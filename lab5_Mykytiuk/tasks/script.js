ff4e20f73c99e53a3cff3e96d6e903ef02a3012d
Ihor Mykytiuk <igor14vn@gmail.com>
2023-10-17 12:42:11 +0300

    Summary

--- lab5_Mykytiuk/tasks/script.js
+++ lab5_Mykytiuk/tasks/script.js
@@ -12,52 +12,20 @@ function tabSwitch(new_tab, new_content) {
          
 }
 //--------------------------------------------------------------
- /*function tabSwitch(active) {
-        let subMenu = document.getElementsByClassName(active);
-        let subMenu1 = document.getElementsByClassName("sub-nav1");
-        let subMenu2 = document.getElementsByClassName("sub-nav2");
-        let subSubMenu2 = document.getElementsByClassName("sub-sub-nav2");
-        console.log(subMenu);
-        if (active == "sub-nav1") {
-          subMenu[0].classList.toggle("sub-menu-active");
-          if (subMenu2[0].classList.contains("sub-menu-active")) {
-            subMenu2[0].classList.remove("sub-menu-active");
-          }
-        }
-        if (active == "sub-nav2") {
-          subMenu[0].classList.toggle("sub-menu-active");
-          if (subMenu1[0].classList.contains("sub-menu-active")) {
-            subMenu1[0].classList.remove("sub-menu-active");
-          }
-          if (subSubMenu2[0].classList.contains("sub-menu-active")) {
-            subSubMenu2[0].classList.remove("sub-menu-active");
-          }
-        }
-        if (active == "sub-sub-nav2") {
-          subMenu[0].classList.toggle("sub-menu-active");
-        }
-      }*/
+function tabSwitch(active) {
+  let activeMenu = document.querySelector(`.${active}`);
+  let subMenus = document.querySelectorAll(".nav ul");
 
-      /* document.querySelector(".nav").onclick = function (event) {
-        console.log(event.target.nextElementSibling);
-        if (event.target.nodeName !== "A") return;
-
-        event.target.classList.add("sub-menu-active-a");
-      };*/
-      function tabSwitch(active) {
-        let activeMenu = document.querySelector(`.${active}`);
-        let subMenus = document.querySelectorAll(".nav ul");
-
-        subMenus.forEach((menu) => {
-          if (
-            menu.classList.contains("sub-menu-active") &&
-            active !== "sub-sub-nav2" &&
-            !activeMenu.classList.contains("sub-menu-active")
-          ) {
-            menu.classList.remove("sub-menu-active");
-            menu.previousElementSibling.classList.remove("sub-menu-active-a");
-          }
-        });
-        activeMenu.classList.toggle("sub-menu-active");
-        activeMenu.previousElementSibling.classList.toggle("sub-menu-active-a");
-      }
+  subMenus.forEach((menu) => {
+    if (
+      menu.classList.contains("sub-menu-active") &&
+      active !== "sub-sub-nav2" &&
+      !activeMenu.classList.contains("sub-menu-active")
+    ) {
+      menu.classList.remove("sub-menu-active");
+      menu.previousElementSibling.classList.remove("sub-menu-active-a");
+    }
+  });
+  activeMenu.classList.toggle("sub-menu-active");
+  activeMenu.previousElementSibling.classList.toggle("sub-menu-active-a");
+}
