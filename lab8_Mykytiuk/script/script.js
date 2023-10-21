4b88172736f8fad01d85aea1e2e7b1b3d35b0d1b
Ihor Mykytiuk <igor14vn@gmail.com>
2023-10-22 01:27:30 +0300

    task2

--- lab8_Mykytiuk/script/script.js
+++ lab8_Mykytiuk/script/script.js
@@ -23,7 +23,7 @@ let time = {
       this.second %= 60;
     },
   };
-  document.getElementById("add_time").addEventListener("click", () => {
+  document.querySelector("#add_time").addEventListener("click", () => {
     let hours = parseInt(document.getElementById("hours").value);
     let minutes = parseInt(document.getElementById("minutes").value);
     let seconds = parseInt(document.getElementById("seconds").value);
@@ -35,3 +35,111 @@ let time = {
     time.addSeconds(seconds);
     time.displayTime();
   });
+  
+  const months = [
+    "січня",
+    "лютого",
+    "березня",
+    "квітня",
+    "травня",
+    "червня",
+    "липня",
+    "серпня",
+    "вересня",
+    "жовтня",
+    "листопада",
+    "грудня",
+  ];
+  const days = [
+    "неділя",
+    "понеділок",
+    "вівторок",
+    "середа",
+    "четвер",
+    "п'ятниця",
+    "субота",
+  ];
+
+  function currendDate() {
+    now = new Date();
+    hours = now.getHours();
+    minutes = now.getMinutes();
+    day = now.getDay();
+    date = now.getDate();
+    month = now.getMonth();
+    year = now.getFullYear();
+    document.getElementById("date").innerHTML =
+      "Дата: " + date + " " + months[month] + " " + year + " року";
+    document.getElementById("day").innerHTML = "День тижня: " + days[day];
+    document.getElementById("time").innerHTML =
+      "Час: " +
+      (hours < 10 ? "0" : "") +
+      hours +
+      ":" +
+      (minutes < 10 ? "0" : "") +
+      minutes;
+  }
+  function getDayInfo(dateObject) {
+    let dayNumber = dateObject.getDay();
+    let dayName = days[dayNumber];
+    dayNumber = dayNumber === 0 ? 7 : dayNumber;
+    return { dayNumber, dayName };
+  }
+  document.getElementById("num-and-name_button").onclick = function () {
+    let dateObject = new Date();
+    let dayInfo = getDayInfo(dateObject);
+    document.getElementById("day_num").innerHTML =
+      "Номер дня тижня: " + dayInfo.dayNumber;
+    document.getElementById("day_name").innerHTML =
+      "Назва дня тижня: " + dayInfo.dayName;
+  };
+
+  document.getElementById("find-date_button").onclick = function () {
+    let daysN = document.getElementById("days_n").value;
+    let dateN = new Date();
+    let dateCase =
+      daysN > 0
+        ? "Майбутня дата: "
+        : daysN < 0
+        ? "Минула дата: "
+        : "Сьогодні: ";
+
+    dateN.setDate(dateN.getDate() + parseInt(daysN));
+    document.getElementById("date_n").innerHTML =
+      dateCase +
+      dateN.getDate() +
+      " " +
+      months[dateN.getMonth()] +
+      " " +
+      dateN.getFullYear() +
+      " року";
+  };
+
+  function getLastDayOfMonth(year, month) {
+    let lastDay = new Date(year, month, 0);
+    return lastDay.getDay();
+  }
+  document.getElementById("find-last-day_button").onclick = function () {
+    let year = document.getElementById("year").value;
+    let month = document.getElementById("month").value;
+    let lastDay = getLastDayOfMonth(year, month);
+    document.getElementById("last-day").innerHTML =
+      "Останній день місяця: " + days[lastDay];
+  };
+  function getSeconds() {
+    now = new Date();
+    hours = now.getHours();
+    minutes = now.getMinutes();
+    seconds = now.getSeconds();
+    secondsNow = hours * 3600 + minutes * 60 + seconds;
+    secondsLeft = 86400 - secondsNow;
+    return { secondsNow, secondsLeft };
+  }
+  document.getElementById("find-seconds_button").onclick = function () {
+    let seconds = getSeconds();
+    document.getElementById("seconds_now").innerHTML =
+      "Секунд пройшло: " + seconds.secondsNow;
+    document.getElementById("seconds_left").innerHTML =
+      "Секунд залишилось: " + seconds.secondsLeft;
+  };
+  
